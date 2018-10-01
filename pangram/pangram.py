72713c845f68c7bf1f72ab4702e0bd58b14ce0e9
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-01 15:56:07 -0400

    solved pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,9 @@
 def is_pangram(sentence):
-    pass
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    sentence_set = set(sentence.lower())
+
+    for letter in alpha:
+        if not (letter in sentence_set):
+            return False
+    return True
+
