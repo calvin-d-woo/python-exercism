4e990a4651b19ed824eeb6dae940c5f06c873576
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-01 15:12:38 -0400

    finished leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,7 @@
 def is_leap_year(year):
-    pass
+    if year < 0:
+        raise Exception("Not a valid year. Make it positive!")
+    else:
+        if (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)):
+            return True
+        return False
