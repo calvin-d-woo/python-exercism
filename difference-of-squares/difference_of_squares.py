fe8c78e52801d857ff42a3d9ce88c0657d00eb01
Calvin D. Woo <calvin.d.woo@gmail.com>
2018-10-02 12:39:42 -0400

    finished diff-of-squares exercise

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,14 @@
 def square_of_sum(count):
-    pass
+    sum_of_count = count * (count + 1) / 2
+    return sum_of_count**2
 
 
 def sum_of_squares(count):
-    pass
+    sum = 0
+    for n in range(count):
+        sum += (n+1)**2
+    return sum
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
